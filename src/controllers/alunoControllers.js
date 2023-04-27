5128c8ca270cf5909eed87bc41685228046acf6b
techchoi <ninahjc.94@gmail.com>
2023-04-27 00:05:10 -0300

    separando as responsabilidades do express

--- src/controllers/alunoControllers.js
+++ src/controllers/alunoControllers.js
@@ -1,13 +1,13 @@
 const express = require('express') //importando as dependencias do express
 const app = express() //criando objeto do express na variavel app
 const port = 3000
+const router = express.Router();
 
-app.get('/',(req, res) => {
+
+//exemplo simples de hello world
+router.get('/',(req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`O servidor está de ON na porta ${port}`)
-})
 
-
+module.exports = router;
