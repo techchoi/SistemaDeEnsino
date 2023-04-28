ee9bec33844eb8497700603f516eb0572d0e4c21
techchoi <ninahjc.94@gmail.com>
2023-04-28 18:05:49 -0300

    Criando controller usuario

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const app = express(); //criando um objeto para o express
 app.use(bodyParser.json()); //converte o corpo para json
 
 const aluno = require('./src/controllers/alunoControllers');
+const usuario = require('./src/controllers/usuarioControllers')
 
 app.use('/api/aluno', aluno); //rota do local host
+app.use('/api/usuario', usuario); //rota do usuario 
 
 module.exports = app; // exportando o módulo app que é o express
 
---
+++ src/controllers/usuarioControllers.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const router = express.Router();
+
+router.get('/',(req,res) => {
+    res.send('Seja bem vindo !');
+})
+
+router.post('/', (req, res) => {
+    const nome = req.body;
+    console.log(req.body);
+    res.json(req.body);
+})
+
+router.delete('/', (req, res) => {
+    res.send('Usuário deletado!');
+})
+
+router.put('/', (req, res) => {
+    res.send('Usuário alterado com sucesso !')
+})
+
+module.exports = router; 
+
